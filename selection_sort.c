7363feeb9c95abb35bda686cdc9976e19591a40c
Dhiraj Patra <dhiraj.patra@gmail.com>
2025-07-18 12:53:44 +0530

    updated

---
+++ selection_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void selection_sort(int arr[], int n) {
+    int i, j, min_index, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        min_index = i;  
+
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+
+        temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+}
+
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
